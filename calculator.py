d3fb7ac844d0be4a42882f2eef55f73a8248e957
sanjay3234 <sanjayapandit1111@gmail.com>
2019-11-17 17:54:59 +0545

    begineer

--- calculator.py
+++ calculator.py
@@ -1,67 +1,47 @@
-Username=input("Enter your Username =")
-Password =input("Enter your Password2 =")
-if Username=="SANJAY" and Password =="PANDIT":
-    print("WELCOME",Username,Password,"NOW YOU CAN GO FOR FURTHER PROCESSING!")
-else:
-    print("Plesae check yor UserName and Password")
-number1=int(input("Enterd first number ="))
-number2=int(input("Enterd second number ="))
-sum=number1+number2
-value=int(input("enter the sum of number1 and number2 ="))
-if sum==value:
-        print("you can use calculator")
-else:
-        print("you cannot use calculator")
-def Add(num1,num2):
-    return num1+num2
-def Subtract(num1,num2):
-    return num1-num2
-def Multiply(num1,num2):
-    return num1*num2
-def Divide(num1,num2):
-    return num1/num2
-def Quotient(num1,num2):
-    return num1//num2
-def Reminder(num1,num2):
-    return num1%num2
-def power(num1,num2):
-    return num1**num2
-import math
-def sqrt(num1):
-    return math.sqrt(num1)
+name=input("PLEASE ENTER YOUR NAME =")
+while name.isalpha()==False or len(name)<8 :
+    if ' ' in name :
+        break
+    else:
+        print("THE NAME YOU ENTERED IS INCORRECT")
+    name=input("ENTER THE NAME AGEIN =")
+print("HELLO SIR ! NOW YOU CAN USE THE CALCULATOR FOR FOLLOWING OPERATIONS")
 print("Select the operation you want to perform")
-print("1.Add    2.Subtract     3.Multiply      4.Divide     5.Reminder     6.Quotient      7.Power      8.Square Root")
-Choice=int(input("Enter your choice(1/2/3/4/5/6/7/8):"))
+print("1.Add    2.Subtract     3.Multiply      4.Divide     5.Reminder     6.Quotient      7.Power")
+Choice=int(input("Enter your choice(1/2/3/4/5/6/7):"))
 if Choice == 1:
     num1 = int(input("Enter first number ="))
     num2 = int(input("Enter second number ="))
+    sum=num1+num2
     print("Sum of num1 and num2 =",num1+num2)
 elif Choice == 2:
     num1=int(input("Enter first number ="))
     num2=int(input("Enter second number ="))
+    Difference=num1-num2
     print("Difference of num1 and num2 =", num1 - num2)
 elif Choice == 3:
     num1=int(input("Enter first number ="))
     num2=int(input("Enter second number ="))
+    Multiplication=num1*num2
     print("Multiplication of num1 and num2 =", num1 * num2)
 elif Choice == 4:
     num1=int(input("Enter first number ="))
     num2=int(input("Enter second number ="))
+    Divide=num1/num2
     print("Division of num1 and num2 =", num1 / num2)
 elif Choice == 5:
     num1=int(input("Enter first number ="))
     num2=int(input("Enter second number ="))
+    Quotiend=num1//num2
     print("Quotient after dividing num1 and num2 =", num1 // num2)
 elif Choice == 6:
     num1=int(input("Enter first number ="))
     num2=int(input("Enter second number ="))
+    Reminder=num1%num2
     print("Reminder after dividing num1 and num2 =", num1 % num2)
 elif Choice == 7:
     num1=int(input("Enter first number ="))
     num2=int(input("Enter second number ="))
+    power=num1**num2
     print("Power of num1 by num2 =", num1 ** num2)
-elif Choice == 8:
-    num=int(input("Enter first number ="))
-    print("Square Root of first number =", sqrt(num))
-else:
-    print("Choice of opreation out of range")
+
